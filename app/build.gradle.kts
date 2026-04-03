fdb4cb9703af3542c781813e39ebf8e3456f954a
stefano <stefanomarocco0@gmail.com>
2026-04-04 01:47:11 +0200

    chore: update Gradle and library versions for improved compatibility

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -81,9 +81,9 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.icons.core)
 
     implementation(libs.hilt.android)
-    implementation(libs.androidx.material3)
     ksp(libs.hilt.compiler)
     implementation(libs.hilt.navigation.compose)
     implementation(libs.navigation.compose)
